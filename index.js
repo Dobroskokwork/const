5380a8b7e13dfc74bd0077178c7cad81851c2918
Dobroskokwork <105386218+Dobroskokwork@users.noreply.github.com>
2022-07-14 16:13:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let myName = 'Михаил';
+let mySurname = 'Доброскок';
+let myFavoriteDrink = "Coca-cola";
+let myFavoriteAnimal = 'Собаки';
+let myFavoriteProgrammingLanguage = 'Pascal';
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
